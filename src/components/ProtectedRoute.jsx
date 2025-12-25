239e7819a31b873e0925ffe8d20782745cef6391
TsiOn <tshimelis23@gmail.com>
2025-12-25 18:34:29 +0300

    updated protected route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,22 @@
 import { Navigate } from "react-router-dom";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
+import { useEffect, useState } from "react";
 
 export default function ProtectedRoute({ children }) {
-  const [user, loading] = useAuthState(auth);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  // ⏳ WAIT for Firebase to load
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  // ⏳ Wait for Firebase
   if (loading) {
     return (
       <div className="h-screen flex items-center justify-center">
